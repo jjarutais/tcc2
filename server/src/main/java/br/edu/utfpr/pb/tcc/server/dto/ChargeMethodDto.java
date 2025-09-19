c4b3aa2020753dbdd2cf788da2110d11840dc656
João Gabriel Jarutais <129345308+jjarutais@users.noreply.github.com>
2025-09-18 22:59:54 -0300

    cadastro de caixas/ contas e métodos de pagamento

---
+++ server/src/main/java/br/edu/utfpr/pb/tcc/server/dto/ChargeMethodDto.java
@@ -0,0 +1,24 @@
+package br.edu.utfpr.pb.tcc.server.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChargeMethodDto {
+
+    private Long id;
+
+    @NotNull
+    @Size(min = 2, max = 100)
+    private String description;
+
+    @NotNull
+    private Boolean active;
+}
---
+++ server/src/main/java/br/edu/utfpr/pb/tcc/server/service/IChargeMethodService.java
@@ -0,0 +1,6 @@
+package br.edu.utfpr.pb.tcc.server.service;
+
+import br.edu.utfpr.pb.tcc.server.model.ChargeMethod;
+
+public interface IChargeMethodService extends ICrudService<ChargeMethod, Long> {
+}
